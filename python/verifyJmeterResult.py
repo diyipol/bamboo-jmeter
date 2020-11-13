9e5345f2fb0b917bbe1a4f03911761f7645a66b4
Pablo Betancor <p.betancor@gmail.com>
2020-11-13 13:53:04 +0100

    Parametrized python script

--- python/verifyJmeterResult.py
+++ python/verifyJmeterResult.py
@@ -1,9 +1,19 @@
 import pandas as pd
 import sys
 
-print("Summary of jmeter execution")
+if len(sys.argv) < 3:
+    print("Script has three args: jmeterResultFile, maxExecutionTimeAllowed and maxNumberSlowerExecutionsAllowed")
+    sys.exit(-1)
 
-jmeterResult = pd.read_csv("./jmeter-result.jtl")
+jmeterResultFile = sys.argv[1]
+maxExecutionTimeAllowed = int(sys.argv[2])
+maxNumberSlowerExecutionsAllowed = int(sys.argv[3])
+
+print("Verify jmeter tests result with jmeterResultFile [", 
+    jmeterResultFile, "], maxExecutionTimeAllowed [", maxExecutionTimeAllowed, 
+    "] and maxNumberSlowerExecutionsAllowed [", maxNumberSlowerExecutionsAllowed, "]")
+
+jmeterResult = pd.read_csv(jmeterResultFile)
 
 okExecutions = jmeterResult[jmeterResult["success"] == True]["success"].count()
 
@@ -15,11 +25,11 @@ print("There are [", koExecutions, "] error executions in jmeter result")
 
 print("The average response time is [", jmeterResult["elapsed"].mean(), "]")
 
-slowerExecutions = jmeterResult[jmeterResult["elapsed"] > 300]["elapsed"].count()
+slowerExecutions = jmeterResult[jmeterResult["elapsed"] > maxExecutionTimeAllowed]["elapsed"].count()
 
 print("There are [", slowerExecutions, "] slower executions in jmeter result")
 
-if koExecutions > 0 or slowerExecutions > 0 :
+if koExecutions > 0 or slowerExecutions > maxNumberSlowerExecutionsAllowed :
     sys.exit(-1)
 
 sys.exit(0)
